3ab1b97a7ab1ed76ee0e78406e7898c9253ab278
levan1c <laga.manaois.up@phinmaed.com>
2024-02-24 01:18:00 +0800

    testing
    git commit -m testing

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+
+        // YAINT ORIGINALLY HERE
+        'payment/*', // exclude all URLs with payment/ prefix
+        'user/add' // exclude exact URL
     ];
 }
